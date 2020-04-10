2e4139555511414c5504ec980d31bb6eac57389b
apsshrinet <63453591+apsshrinet@users.noreply.github.com>
2020-04-10 17:06:43 +0530

    first upload

---
+++ action_page.php
@@ -0,0 +1,11 @@
+<html>
+<body>
+
+action_page <?php echo $_GET["name"]; ?><br>
+Your email address is: <?php echo $_GET["email"]; ?>
+Your age is: <?php echo $_GET["age"]; ?>
+Your registered club name is: <?php echo $_GET["club name"]; ?>
+<h4>"Thankyou! Your data is saved to us."</h4>
+
+</body>
+</html>
